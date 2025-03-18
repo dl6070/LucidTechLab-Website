74b5bef793b50c68ee4dd1b650142fc6dc1718ef
dl6070 <dlucid3@gmail.com>
2025-03-18 17:01:11 -0500

    Committed: Testing

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,17 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
-  darkMode: 'class',
   theme: {
     extend: {
       colors: {
-        orange: {
-          500: '#FF8300',
-          600: '#E67600'
+        'brand': {
+          green: '#16a34a',
+          blue: '#2563eb',
         }
+      },
+      container: {
+        center: true,
+        padding: '1rem',
       }
     },
   },
